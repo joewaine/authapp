0f3a9dfc8075a8468d8a8d9c7648b0b5b3da9274
joe waine <joewaine@wawa1114s-imac.local>
2020-06-05 18:19:29 -0700

    nod

---
+++ server/api/user/route/user.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../../config/auth");
+const userController = require("../controller/userController");
+
+router.post("/register", userController.registerNewUser);
+router.post("/login", userController.loginUser);
+
+
+router.post("/addproduct", userController.addProduct);
+
+router.get("/me", auth, userController.getUserDetails);
+
+module.exports = router;
